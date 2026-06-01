c638857db1c0a833922aa7f45ff5cb357bbbbc50
Elephenman <217536768+Elephenman@users.noreply.github.com>
2026-06-01 15:02:21 +0800

    feat: add theme and MainActivity

---
+++ app/src/main/java/com/elephenman/docviewer/ui/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.elephenman.docviewer.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.elephenman.docviewer.ui.theme.DocViewerTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DocViewerTheme {
+                // Will be replaced with navigation in later tasks
+            }
+        }
+    }
+}
